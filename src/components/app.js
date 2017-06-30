082dcdf7e8dd0b983293dc788a3bb6dc40611b68
bvluong <bryantvinluong@gmail.com>
2017-06-29 19:21:58 -0700

    Initially commit

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <h1>It Works</h1>
+        React simple starter
+      </div>
     );
   }
 }
